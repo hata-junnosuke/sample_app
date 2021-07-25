a36f7171251c1ec05516465ae043bc83dbf0d70f
hata-junnosuke <jh19911209@icloud.com>
2021-07-25 22:37:27 +0000

    10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,19 +2,20 @@ class SessionsController < ApplicationController
   def new
   end
   
-  def create
+ def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
     else
-      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない# エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
-  end
+ end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
